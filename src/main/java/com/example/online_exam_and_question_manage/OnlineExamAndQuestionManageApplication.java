4d75ef5fdaa45f72c02851528b52734c7ae05ac0
AGardon <wyx2603494062@126.com>
2021-11-28 13:18:30 +0800

    Initial commit

---
+++ src/main/java/com/example/online_exam_and_question_manage/OnlineExamAndQuestionManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_exam_and_question_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamAndQuestionManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamAndQuestionManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/online_exam_and_question_manage/OnlineExamAndQuestionManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_exam_and_question_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamAndQuestionManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
